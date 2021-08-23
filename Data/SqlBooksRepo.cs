38d6c842271c71ac69664ec38b8da96aa4e03e3a
derciosinione <derciosinione@gmail.com>
2021-08-24 00:26:14 +0100

    feat: Create Books Repository

---
+++ Data/SqlBooksRepo.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
+using back_end_challenge.Models;
+
+namespace back_end_challenge.Data
+{
+  public class SqlBooksRepo : IBookRepo
+  {
+    public CoreContext _context { get; }
+    public SqlBooksRepo(CoreContext context)
+    {
+      _context = context;
+    }
+
+
+    public Books GetBookById(int id)
+    {
+      return _context.Books.FirstOrDefault(x => x.Id == id);
+    }
+
+    public IEnumerable<Books> GetBooks()
+    {
+      return _context.Books.ToList();
+    }
+  }
+}
