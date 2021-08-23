5474123fe42ccb81fb651219cfc6834875d451f8
derciosinione <derciosinione@gmail.com>
2021-08-23 19:11:43 +0100

    feat: Create Book Models

---
+++ Models/Livros.cs
@@ -0,0 +1,12 @@
+namespace back_end_challenge.Models
+{
+  public class Livros
+  {
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public string Genero { get; set; }
+    public string NumCopias { get; set; }
+
+  }
+}
