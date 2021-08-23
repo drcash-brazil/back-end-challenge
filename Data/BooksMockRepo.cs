1622f78e4121e0118588c2f1c8fc800b0a2d470e
derciosinione <derciosinione@gmail.com>
2021-08-23 19:30:20 +0100

    feat: Get started with Static data

---
+++ Data/BooksMockRepo.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace back_end_challenge.Data
+{
+  public class Books : IBookRepo
+  {
+    public Books GetBookById(int id)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public IEnumerable<Books> GetBooks()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
