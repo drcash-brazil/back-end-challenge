abfdb4ddbf2afda991f57459fc02972e46132f31
derciosinione <derciosinione@gmail.com>
2021-08-23 19:41:03 +0100

    feat: Books dummi data

--- Data/BooksMockRepo.cs
+++ Data/BooksMockRepo.cs
@@ -1,17 +1,23 @@
 using System.Collections.Generic;
+using back_end_challenge.Models;
 
 namespace back_end_challenge.Data
 {
-  public class Books : IBookRepo
+  public class BooksMockRepo : IBookRepo
   {
     public Books GetBookById(int id)
     {
-      throw new System.NotImplementedException();
+      return new Books { Id = 1, Autor = "Dércio Derone", Genero = "Romance" };
     }
 
     public IEnumerable<Books> GetBooks()
     {
-      throw new System.NotImplementedException();
+      var books = new List<Books>(){
+        new Books { Id = 1, Autor = "Dércio Derone", Genero = "Romance" },
+        new Books { Id = 2, Autor = "Dr. Cash", Genero = "IT" },
+        new Books { Id = 3, Autor = "Kiari Code", Genero = "Empreendedorismo" },
+      };
+      return books;
     }
   }
 }
