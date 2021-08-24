e19766a436e797f5854e26f8c81146641db1b189
derciosinione <derciosinione@gmail.com>
2021-08-24 03:40:00 +0100

    feat: Add Data Annotation into DTO class

--- Dtos/BooksCreateDto.cs
+++ Dtos/BooksCreateDto.cs
@@ -1,11 +1,23 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace back_end_challenge.Dtos
 {
   public class BooksCreateDto
   {
+    [Required]
+    [MaxLength(50)]
     public string Titulo { get; set; }
+
+    [Required]
+    [MaxLength(100)]
     public string Autor { get; set; }
+
+    [Required]
+    [MaxLength(20)]
     public string Genero { get; set; }
+
+    [Required]
     public int NumCopias { get; set; }
 
   }
