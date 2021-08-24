eadc91eca5efae506f0b95a26c8b04fa1522fc3d
derciosinione <derciosinione@gmail.com>
2021-08-24 02:00:41 +0100

    feat: Return Mapper objects in our response

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using back_end_challenge.Repositories;
 using back_end_challenge.Models;
 using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using back_end_challenge.Dtos;
 
 namespace back_end_challenge.Controllers
 {
@@ -10,27 +12,30 @@ namespace back_end_challenge.Controllers
   public class BooksController : ControllerBase
   {
     private readonly IBookRepo _repository;
-    public BooksController(IBookRepo repository)
+    private readonly IMapper _mapper;
+
+    public BooksController(IBookRepo repository, IMapper mapper)
     {
       _repository = repository;
+      _mapper = mapper;
     }
 
     // GET api/books/
     [HttpGet]
-    public ActionResult<IEnumerable<Books>> GetAllBooks()
+    public ActionResult<IEnumerable<BooksReadDto>> GetAllBooks()
     {
       var bookItems = _repository.GetBooks();
-      if (bookItems is null) return NotFound();
-      return Ok(bookItems);
+      return Ok(_mapper.Map<IEnumerable<BooksReadDto>>(bookItems));
     }
 
     // GET api/books/{id}
     [HttpGet("{id}")]
-    public ActionResult<IEnumerable<Books>> GetABookById(int id)
+    public ActionResult<IEnumerable<BooksReadDto>> GetABookById(int id)
     {
       var bookItem = _repository.GetBookById(id);
       if (bookItem is null) return NotFound();
-      return Ok(bookItem);
+      return Ok(_mapper.Map<BooksReadDto>(bookItem));
+      // return Ok(bookItem);
     }
   }
 }
