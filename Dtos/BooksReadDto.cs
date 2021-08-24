1aae847d8613e62e5e03217da04dfc7bc6f6ef07
derciosinione <derciosinione@gmail.com>
2021-08-24 01:24:09 +0100

    feat: Create Books DTO

---
+++ Dtos/BooksReadDto.cs
@@ -0,0 +1,16 @@
+
+namespace back_end_challenge.Dtos
+{
+  public class BooksReadDtos
+  {
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+
+    public string Autor { get; set; }
+
+    public string Genero { get; set; }
+
+    public int NumCopias { get; set; }
+
+  }
+}
