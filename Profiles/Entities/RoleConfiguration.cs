a93224a9853bc37dd3f6ab7ac8273b900842e9bc
derciosinione <derciosinione@gmail.com>
2021-08-28 04:44:15 +0100

    feat: Create Roles

---
+++ Profiles/Entities/RoleConfiguration.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace back_end_challenge.Profiles.Entities
+{
+  public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+  {
+    public void Configure(EntityTypeBuilder<IdentityRole> builder)
+    {
+      builder.HasData(
+        new IdentityRole
+        {
+          Name = "User",
+          NormalizedName = "USER"
+        },
+        new IdentityRole
+        {
+          Name = "Administrator",
+          NormalizedName = "ADMINISTRATOR"
+        },
+        new IdentityRole
+        {
+          Name = "Employee",
+          NormalizedName = "EMPLOYEE"
+        }
+      );
+    }
+  }
+}
