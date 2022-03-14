2cb793caa1f4d19f8652cf311b5ed3a65a7958f7
andersonfranciscoig@gmail.com <andersonfranciscoig@gmail.com>
2022-03-14 01:14:24 +0100

    :construction: definição dos repositorios

---
+++ DrcashTest/Repositories/AuthorsRepository.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using back_end_challenge.IRepository;
+using back_end_challenge.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrcashTest.Repositories
+{
+    public class AuthorsRepository : IAuthorsRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthorsRepository(DataContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IList<Authors>> GetAll() => await _context.Authors.AsNoTracking().ToListAsync();
+
+        public async Task<Authors> GetById(int id) => await _context.Authors.FirstOrDefaultAsync(x => x.Id == id);
+       
+        public async Task<Authors> GetByAuthorWithBooks(int id) => 
+            await _context.Authors.Include(b => b.Books)
+                .ThenInclude(b=>b.Categories)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+        public async Task<Authors> GetByName(string name) =>
+            await _context.Authors.FirstOrDefaultAsync(x => x.nome.Contains(name));
+
+        public async Task<Authors> Insert(Authors entity)
+        {
+            _context.Authors.Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<bool> Update(Authors entity)
+        {
+            _context.Authors.Update(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+
+        public async Task<bool> Delete(int id, Authors entity)
+        {
+            _context.Authors.Remove(entity);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
