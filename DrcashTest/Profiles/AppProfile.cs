94c46079f2554929a50324d0f18c1a6f595b5422
andersonfranciscoig@gmail.com <andersonfranciscoig@gmail.com>
2022-03-14 01:09:49 +0100

    :construction_worker: defindo o modlo de perfis para um usuario

---
+++ DrcashTest/Profiles/AppProfile.cs
@@ -0,0 +1,30 @@
+using AutoMapper;
+using back_end_challenge.Models;
+using back_end_challenge.Models.Dtos;
+
+namespace DrcashTest.Profiles
+{
+  public class AppProfile : Profile
+  {
+    public AppProfile()
+    {
+      CreateMap<Books, BooksReadDto>().ReverseMap();
+      CreateMap<Books, BookCreateDto>().ReverseMap();
+      CreateMap<Books, BookUpdateDto>().ReverseMap();
+
+      CreateMap<Category, CategoryReadDto>().ReverseMap();
+      CreateMap<Category, CategoryCreateDto>().ReverseMap();
+      CreateMap<Category, CategoryUpdateDto>().ReverseMap();
+
+      CreateMap<Authors, AuthorReadDto>().ReverseMap();
+      CreateMap<Authors, AuthorCreateDto>().ReverseMap();
+      CreateMap<Authors, AuthorUpdateDto>().ReverseMap();
+
+      CreateMap<Users, UserCreateDto>().ReverseMap();
+      CreateMap<Users, UserReadDto>().ReverseMap();
+      CreateMap<Users, UserUpdateDto>().ReverseMap();
+      CreateMap<Users, LoginUserDto>().ReverseMap();
+
+    }
+  }
+}
