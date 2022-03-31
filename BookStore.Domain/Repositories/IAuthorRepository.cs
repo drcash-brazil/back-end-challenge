3524098625f8a7cce4eddee9f1b2a48f8f3a2024
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-03-31 11:00:54 +0100

    update the type of  repository

--- BookStore.Domain/Repositories/IAuthorRepository.cs
+++ BookStore.Domain/Repositories/IAuthorRepository.cs
@@ -2,5 +2,5 @@ using BookStore.Domain.Entities;
 
 namespace BookStore.Domain.Repositories;
 
-public interface IAuthorRepository : IGenericRepository<Book>
+public interface IAuthorRepository : IGenericRepository<Author>
 { }
