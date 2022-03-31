e6058ed4b92b255d0ea0f8fca6605b3aeb0e7c43
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-03-31 23:00:30 +0100

    ignore elements in json

--- BookStore.Domain/Entities/Author.cs
+++ BookStore.Domain/Entities/Author.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace BookStore.Domain.Entities;
 
 public class Author : Entity
@@ -8,6 +10,12 @@ public class Author : Entity
     }
 
     public string Name { get; private set; } = String.Empty;
+
+    [JsonIgnore]
+    public Guid BookId { get; set; }
+
+    [JsonIgnore]
+    public Book? Book { get; set; }
 
     public void UpdateAuthor(string name)
     {
