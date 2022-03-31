93be0fa5873bf86873629736da6236dd2067f58a
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-03-31 23:00:41 +0100

    update book entity

--- BookStore.Domain/Entities/Book.cs
+++ BookStore.Domain/Entities/Book.cs
@@ -2,37 +2,31 @@ namespace BookStore.Domain.Entities;
 
 public class Book : Entity
 {
-    public Book(string title, int authorId, Author? author, int genreId, Genre? genre, int numberOfCopies)
+    public Book(string title, Guid authorId, Guid genreId, int numberOfCopies)
     {
         Title = title;
         AuthorId = authorId;
-        Author = author;
         GenreId = genreId;
-        Genre = genre;
         NumberOfCopies = numberOfCopies;
     }
 
     public string Title { get; private set; } = String.Empty;
-    public int AuthorId { get; set; }
-    public Author? Author { get; private set; }
-    public int GenreId { get; set; }
-    public Genre? Genre { get; private set; }
+    public Guid AuthorId { get; set; }
+    public Author Author { get; private set; }
+    public Guid GenreId { get; set; }
+    public Genre Genre { get; private set; }
     public int NumberOfCopies { get; private set; }
 
     public void UpdateBook(
         string title,
-        int authorId,
-        Author? author,
-        int genreId,
-        Genre? genre,
+        Guid authorId,
+        Guid genreId,
         int numberOfCopies
     )
     {
         Title = title;
         AuthorId = authorId;
-        Author = author;
         GenreId = genreId;
-        Genre = genre;
         NumberOfCopies = numberOfCopies;
     }
 }
