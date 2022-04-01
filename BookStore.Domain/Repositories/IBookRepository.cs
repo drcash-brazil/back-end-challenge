e89122a02a9fc374ce0f5892957ede8fc0de3fa7
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-04-01 08:13:37 +0100

    remove dependencia on book repository

--- BookStore.Domain/Repositories/IBookRepository.cs
+++ BookStore.Domain/Repositories/IBookRepository.cs
@@ -3,6 +3,4 @@ using BookStore.Domain.Entities;
 namespace BookStore.Domain.Repositories;
 
 public interface IBookRepository : IGenericRepository<Book>
-{
-    IEnumerable<Book> GetBookByAuthor(string author);
-}
+{ }
